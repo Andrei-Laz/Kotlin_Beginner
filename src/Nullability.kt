cdb4081912c41ca6e1e5619ef7e66824802baa27
Andrei-Laz <andlaz2@alu.edu.gva.es>
2025-09-18 20:01:25 +0200

    Version with incomplete nullability

---
+++ src/Nullability.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    //In Kotlin by default variable cannot be NULL
+
+    //var rocks: Int = null
+    //Null cannot be a value of a non-null type 'Int'.
+
+    //To circumvent the problem we can declare a variable with the question mark operator
+
+    var marbles: Int? = null
+
+}
